ca23100ddd954ae1efbacb292e23befa6c3ab189
durgesh253 <jadhavdurgesh256@gmail.com>
2023-05-25 22:22:00 +0530

    tabs project completed

--- my-projects/src/App.js
+++ my-projects/src/App.js
@@ -1,16 +1,54 @@
-
-import React from 'react';
-import Darkmode from './Darkmode';
-import './index.css';
-import image from './image.jpg'
+import React, { useEffect, useState } from 'react'
+ const url = `https://course-api.com/react-tabs-project`
 
 function App() {
-  return (
-   <>
-   <Darkmode  image={image}/>
+    const [lodaing , setLoading] = useState(true);
+    const [jobs, setJobs] = useState([]);
+    const [value , setValue] = useState(0);
+
+    const fetchjobs = async () =>{
+        const response = await fetch(url);
+        const newjobs = await response.json();
+        setJobs(newjobs);
+        setLoading(false);
+    }
+
+  useEffect(() =>{
+   fetchjobs();
+  },[]) 
+
+  if(lodaing){
+    return(
+    <section className='section loading'>
+        <h1>Loading....</h1>
+    </section>
+    )
+  }
    
-   </>
-  );
+  
+  const {title,dates,duties,company} = jobs[value];
+  return (
+    <>
+    <h1 className='text-center text-primary'>Experiance</h1>
+   
+    <div className='btn-container text-center p-3 '>
+        {
+            jobs.map((item,index)=>{
+                return <button className='btn btn-outline-success border' onClick={() =>{setValue(index)}}>{item.company}</button>
+            })
+        }
+
+    </div>
+    <div className='container'>
+    <h2 className='text-danger'>{title}</h2>
+   <button className='btn btn-warning '> <h3>{company}</h3> </button>
+   <h5  className='text-primary'>{dates}</h5>
+   <p className=''>{duties}</p>
+   </div>
+
+
+    </>
+     )
 }
 
-export default App;
+export default App
--- my-projects/src/index.js
+++ my-projects/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
-import FormHandling from './FormHandling';
-
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <FormHandling/>
+    
   </React.StrictMode>
 );
 
