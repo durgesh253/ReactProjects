3cc93c8100444a532ae44fe293fdee02d0942c9c
durgesh253 <jadhavdurgesh256@gmail.com>
2023-07-01 01:42:28 +0530

    form validation in hooks

--- my-projects/src/App.js
+++ my-projects/src/App.js
@@ -1,25 +1,69 @@
-import React from 'react'
-import LoginForm from './Pages/LoginForm'
-import { Route, Routes } from 'react-router-dom'
-import AxiosFetch from './Pages/AxiosFetch'
-import Nav from './Pages/Nav'
-import ReactRedux from './Pages/ReactRedux'
-import { store } from './Redux/Store'
-import Products from './Pages/Products'
-
+import React, { useState } from 'react'
 
 function App() {
+    const [email , setemail] =  useState("");
+    const [password , setPassword] = useState("");
+    const [logindata ,setLoginData] = useState([]);
+  const formSubmit =(e) => {
+    e.preventDefault();
+    if(email && password) {
+      const newData = { id: new Date().getTime(),email :  email , password : password };
+      setLoginData([...logindata , newData]);
+      setemail("");
+      setPassword("");
+    }else{
+      alert("Plss Fill the Data");
+    }
+    }
+    const formDelet = (id) => {
+    const updateData = logindata.filter((item) => item.id !== id);
+      setLoginData(updateData);
+    }
   return (
-<>
-   <Nav/>
-  
-    <Routes>
-      <Route path='/axios' element={<AxiosFetch/>}/>
-      <Route path='/login' element={<LoginForm/>}/>
-      <Route path='/redux'  element={<Products/>}/>
-    </Routes >
+    <>
+    <div className='container tamplete w-50 border border-warning'>
+  <form onSubmit={formSubmit}  >
+  <div className="mb-3 text-light">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" name='email' value={email} autoComplete='off'
+    onChange={(e) => setemail(e.target.value)}
+     className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+  </div>
+  <div className="mb-3 text-light">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" name='password' value={password} 
+    onChange={(e) => setPassword(e.target.value)}
+    autoComplete='off' className="form-control" id="exampleInputPassword1" />
+  </div>
+  <div className="mb-3 form-check text-light">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" onSubmit={formSubmit}   className="btn btn-warning">Submit</button>
+</form>
+
+    </div>
+    <div className='container bg-primary   text-light mt-3'>
+      {
+        logindata.map((item) => {
+          const {email,id,password} = item
+          return(
+            <>
+            <div key={id}>
+            <h1>Email: {email}</h1>
+            <h2>Password: {password}</h2>
+            <button type='submit' onClick={() => formDelet(id)} className='btn btn-danger'>Deleate</button>
+            </div>
+            </>
+          )
+
+        })
+      }
+    </div>
     </>
   )
 }
 
 export default App
+
--- my-projects/src/index.js
+++ my-projects/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
 import index from '../src/index.css'
-import { Provider } from 'react-redux';
-import { store } from './Redux/Store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render( <BrowserRouter> <Provider store={store}><App /> </Provider> </BrowserRouter> );
+root.render( <App /> );
 
