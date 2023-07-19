72259f36ed6694dddce8dfea1df0282d634757d4
durgesh253 <jadhavdurgesh256@gmail.com>
2023-07-19 11:36:10 +0530

    movies Clone

--- my-projects/src/componants/MoviDetail.jsx
+++ my-projects/src/componants/MoviDetail.jsx
@@ -13,16 +13,18 @@ useEffect(() => {
 
   return (
     <>
-   <div class="card mb-3" style ={{maxWidth: "80%" , backgroundColor : "black" , color : "white"}}>
+   <div class="card mb-3" style ={{maxWidth: "100%" , backgroundColor : "black" , color : "white"}}>
   <div class="row g-0">
-    <div class="col-md-4">
-      <img src={`https://image.tmdb.org/t/p/original${movieDeatil ? movieDeatil.poster_path : ""}`} class="img-fluid rounded-start" alt="..." />
+    <div class="col-md-4 pt-2">
+      <img src={`https://image.tmdb.org/t/p/original${movieDeatil ? movieDeatil.poster_path : ""}`} class="img-fluid rounded-start h-75 p-lg-3" alt="..." />
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">{movieDeatil.original_title}</h5>
-        <p class="card-text">{movieDeatil.overview}</p>
-        <p class="card-text">{movieDeatil.vote_average}</p>
+        <h5 class="card-title" style={{fontSize:"40px"}}>{movieDeatil.original_title}</h5>
+        <p class="card-text"><h3>About Movie:</h3>{movieDeatil.overview}</p>
+        <h3 class="card-text">Rating: {movieDeatil.vote_average}</h3>
+        <h4>Language : {movieDeatil.original_language}</h4>
+        <h3>Relese Date : {movieDeatil.release_date}</h3>
       </div>
     </div>
   </div>
