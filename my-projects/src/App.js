b79dbb3e0c7c234b10c7f5aa918b657312aa8bed
durgesh253 <jadhavdurgesh256@gmail.com>
2023-08-01 11:08:31 +0530

    todo list app completed

--- my-projects/src/App.js
+++ my-projects/src/App.js
@@ -3,11 +3,19 @@ import TodoList from './TodoList'
 
 function App() {
   return (
+    <>
     <div>
       <h3 className='text-center'>To do List App</h3>
       <TodoList/>
       
     </div>
+    <div className='element'>
+         <p>centered Text</p>
+    </div>
+    <div className='tds'>
+      <div>heelo Durgesh</div>
+    </div>
+    </>
   )
 }
 
