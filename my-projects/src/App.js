483cd83767757533d41f62ab258d0852cc32d37f
durgesh253 <jadhavdurgesh256@gmail.com>
2023-08-13 15:38:38 +0530

    finished

--- my-projects/src/App.js
+++ my-projects/src/App.js
@@ -3,23 +3,37 @@ import data from './data'
 import Category from './Category';
 import Menu from './Menu';
 
-const allcategories =  ["all",...new Set(data.map((item) => item.category))];
-console.log(allcategories)
-function App() {
-  const[menu,setmenu] = useState(data);
-  const [categoryes,setcategoyes]= useState(allcategories)
-  console.log(data);
-  
+// const allcategories =  ["all",...new Set(data.map((item) => item.category))];
 
+
+const allcategories = ["all",...new Set(data.map((item) => item. category))]
+console.log(allcategories);
+function App() {
+  const [menu,setmenu] = useState(data);
+  const[categoryes,setcategoyes] = useState(allcategories);
   const filtercategory = (category) => {
-    if(category === "all") {
+    if(category === "all"){
       setmenu(data);
       return
     }
-    
-    const newdata = data.filter((item) => item.category === category);
-      setmenu(newdata);
+
+    const newdata = data.filter((item) =>  item.category === category);
+    setmenu(newdata)
   }
+  // const[menu,setmenu] = useState(data);
+  // const [categoryes,setcategoyes]= useState(allcategories)
+  // console.log(data);
+  
+
+  // const filtercategory = (category) => {
+  //   if(category === "all") {
+  //     setmenu(data);
+  //     return
+  //   }
+    
+  //   const newdata = data.filter((item) => item.category === category);
+  //     setmenu(newdata);
+  // }
   return (
     <div className=''>
           <h1 className='text-center' style={{borderBottom:"5px solid green" ,width:"120px"}}>Our Menus</h1>
