6a7591e7d1c6656f64728a25c46660388f89a144
durgesh253 <jadhavdurgesh256@gmail.com>
2023-08-28 21:16:08 +0530

    react components state props completed

--- my-projects/src/App.js
+++ my-projects/src/App.js
@@ -1,31 +1,10 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './Navbar'
-import Home from './pages/Home'
-import Coktail from './pages/Coktail'
-import CocktailDetails from './pages/CocktailDetails'
-import PrivateRoute from './pages/PrivateRoute'
-import Login from './pages/Login'
-
-
+import Counter from './Counter'
 
 function App() {
- 
   return (
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/coktails' element={<Coktail/>}/>
-        <Route path='/coktails/:id' element={<PrivateRoute><CocktailDetails/></PrivateRoute>} />
-        <Route path='/product' element={<h1>Product list</h1>} />
-        <Route path='/add-product' element={<h1>Add product</h1>}/>
-        <Route path='/edit-product' element={<h1>Edit product</h1>}/>
-        <Route path='/login' element={<Login/>}/>
-    
-      </Routes>
-     
-      
+      <Counter/>
     </div>
   )
 }
